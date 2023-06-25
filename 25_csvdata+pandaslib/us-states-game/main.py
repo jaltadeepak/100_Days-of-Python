74be3b4d3ef16209b365c607f53fd5ef8a6b6f9e
jaltadeepak <deepaksingh131102@gmail.com>
2023-06-26 00:18:23 +0530

    python

--- 25_csvdata+pandaslib/us-states-game/main.py
+++ 25_csvdata+pandaslib/us-states-game/main.py
@@ -12,17 +12,24 @@ screen.addshape(image)
 turtle = Turtle()
 turtle.shape(image)
 
+screen.screensize(turtle.width, turtle.height)
+
 writer = Turtle()
 writer.hideturtle()
 writer.penup()
 
 data = pandas.read_csv(r"25_csvdata+pandaslib\us-states-game\50_states.csv")
 score = 0
+guessed_states = []
 
 game_is_on = True
 while game_is_on:
     answer_state = screen.textinput(title=f"{score}/50 Guess the State", prompt="What's another state?")
     answer_state = answer_state.title()
+
+    if answer_state == "Exit":
+        game_is_on = False
+        continue
 
     flag = 0
     for state in data.state:
@@ -31,18 +38,31 @@ while game_is_on:
             break
 
     if flag == 1:
+        guessed_states.append(answer_state)
         score += 1
         data_row = data[data.state == answer_state]
-        state = str(data_row.state.values[0])
+        state = data_row.state.values[0]
         print(state)
-        writer.goto(int(data_row.x), int(data_row.y))
+        writer.goto(int(data_row.x.item()), int(data_row.y.item()))
         writer.write(f"{state}", align = ALIGN, font = FONT)
 
     if score == 50:
         writer.clear()
         writer.goto(0, 0)
         writer.write("Game Won.", align=ALIGN, font=FONT)
+        game_is_on = False
 
+missed_state = []
 
+#states_to_learn.csv
+for state in data.state:
+    flag = 0
+    for guessed_state in guessed_states:
+        if state == guessed_state:
+            flag = 1
+    if flag == 0:
+        missed_state.append(state)
 
-
+new_data = pandas.Series(missed_state)
+new_data.to_csv(r"25_csvdata+pandaslib\us-states-game\states_to_learn.csv")
+    
